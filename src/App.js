a721ed2fff1e34c7f67928b40ee1d5b9f2fce9b6
IVB107 <ianvb107@gmail.com>
2019-06-26 13:01:32 -0500

    Added 'Home' component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/home/Home'
 
 function App() {
   return (
     <div className="App">
       <p>Welcome to my Portfolio</p>
+      <Home/>
     </div>
   );
 }
