0cea5dcf3cb0c14cd34854b4fc5760b06a0652a8
IVB107 <ianvb107@gmail.com>
2019-06-26 16:43:34 -0500

    Added scss files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import './App.css';
+import './scss/App.scss';
 import Home from './components/home/Home'
 import Work from './components/work/Work'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
+
+// const sass = require('node-sass')
+// const result = sass.renderSync({
+//   data: './App.scss'
+// })
 
 function App() {
   return (
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Styled from 'styled-components'
+
+const Container = Styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #386C99;
+`;
+const TitleContainer = Styled.div`
+  
+`;
 
 const Home = () => {
   return (
-    <div>
-      Home Component
-    </div>
+    <Container>
+      <TitleContainer>
+
+      </TitleContainer>
+    </Container>
   )
 }
 
