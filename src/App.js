1a17c19f4b2bb145736c2cbc4a58d9727543e67d
IVB107 <ianvb107@gmail.com>
2019-06-28 18:26:57 -0500

    Added styled components to Home.jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-// import './sass/_app.sass';
 import './index.css'
 import Home from './components/home/Home'
 import Work from './components/work/Work'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
-
-// const sass = require('node-sass')
-// const result = sass.renderSync({
-//   data: './App.scss'
-// })
 
 function App() {
   return (
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,30 +1,75 @@
 import React from 'react';
-// import Styled from 'styled-components'
-import '../../sass/_home.sass'
-
-// const Container = Styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   height: 100vh;
-//   background-color: #386C99;
-// `;
-// const TitleContainer = Styled.div`
-
-// `;
+import Styled from 'styled-components'
 
 const Home = () => {
   return (
-    <div>
-      This is a div
-    </div>
-    // <Container>
-    //   <TitleContainer>
-
-    //   </TitleContainer>
-    // </Container>
+    <Container>
+      <TitleContainer>
+        <MainText >
+          <h1>This is some text that will be displayed when you get to my portfolio.</h1>
+        </MainText>
+        <LinkContainer>
+          <p>Links</p>
+          <p>Go</p>
+          <p>Here</p>
+        </LinkContainer>
+        <ScrollArrow>
+          <p>Click me to scroll down</p>
+        </ScrollArrow>
+      </TitleContainer>
+    </Container>
   )
 }
+
+const Container = Styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #386C99;
+`;
+const TitleContainer = Styled.div`
+  background-color: #FFCA25;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 80%;
+  width: 80%;
+  padding: none;
+  margin: none;
+`;
+const MainText = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  felx-direction: column;
+  border: 2px solid black;
+  width: 100%;
+
+  h1 {
+    margin: 1rem;
+    color: #fff;
+  }
+`;
+const LinkContainer = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  border: 2px solid black;
+  width: 100%;
+
+  p {
+    border-bottom: 2px solid white;
+  }
+`;
+const ScrollArrow = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid black;
+  width: 100%;
+`;
 
 export default Home
